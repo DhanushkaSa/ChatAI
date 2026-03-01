e42da4d81b048929bb24a41291cd9ced16922766
DhanushkaSa <dhanushkasa45@gmail.com>
2026-03-02 02:10:42 +0530

    Add env file

--- src/api/getHugginfaceResponse.tsx
+++ src/api/getHugginfaceResponse.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { HUGGING_FACE_KEY } from '../keys/keys';
+import { HUGGING_FACE_KEY } from '@env';
 
 
 
--- src/keys/keys.ts
+++
@@ -1,3 +0,0 @@
-export const HUGGING_FACE_KEY="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"
-
-// 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
